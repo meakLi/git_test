98463321b2b93a1d07f91e8e1be2c3bce15410cf
meakLi <meak.li@silksoftware.com>
2022-10-11 17:09:36 +0800

    列表生成式

--- test2.py
+++ test2.py
@@ -7,5 +7,30 @@
 # new_lis = [num for num in lis if num % 3 == 0]
 # print(new_lis)
 
-new_lis = [num for num in range(30) if num %3 == 0]
+new_lis = [num for num in range(30) if num % 3 == 0]
 print(new_lis)
+
+# listdemo = ['Google','Runoob', 'Taobao']  将列表中各字符串值为键，各字符串的长度为值，组成键值对
+listdemo = ['Google', 'Runoob', 'Taobao']
+newdict = {str: len(str) for str in listdemo}
+print(newdict)
+
+# 提供三个数字，以三个数字为键，三个数字的平方为值来创建字典：
+lis = [2, 6, 4]
+newdict1 = {num: num ** 2 for num in lis}
+print(newdict1)
+
+
+
+# list1 = ['python', 'test1', 'test2']
+# list2 = [word.title() if word.startswith('p') else word.upper() for word in list1]
+# print(list2)
+
+list1 = ['python', 'test1', 'test2']
+# for word in list1:
+#     print(word.title())  # 标题化，以大写字母开始
+#     print(word.startswith('p'))  # 判断首字母是不是p
+#     print(word.upper())  # 全部字母大写
+# p开头的str首字母大写，不是p开头的str全部字母大写
+list2 = [word.title() if word.startswith('p') else word.upper() for word in list1]
+print(list2)
